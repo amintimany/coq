e15faa3f9e9251fa582533d6577635b502cc2b73
Amin Timany <amintimany@gmail.com>
2017-05-05 13:33:18 +0200

    Change the option to Set Inductive Cumulativity
    This requires to change the status of Inductive (we have also changed
    CoInductive and Variant) from keyword to identifier.

--- vernac/vernacentries.ml
+++ vernac/vernacentries.ml
@@ -1365,7 +1365,7 @@ let _ =
   declare_bool_option
     { optdepr  = false;
       optname  = "inductive cumulativity";
-      optkey   = ["Ind"; "Cumulativity"];
+      optkey   = ["Inductive"; "Cumulativity"];
       optread  = Flags.is_inductive_cumulativity;
       optwrite = Flags.make_inductive_cumulativity }
 
